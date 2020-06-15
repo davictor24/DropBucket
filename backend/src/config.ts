40c28aebb65c28b305b8225ef96c0eacdc0087f6
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-15 17:12:26 +0100

    Stubbed serverless.yml

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
     filesTable: process.env.FILES_TABLE,
-    createdAtIndex: process.env.CREATED_AT_INDEX,
+    uploadedAtIndex: process.env.UPLOADED_AT_INDEX,
     s3Bucket: process.env.FILES_BUCKET
 }
