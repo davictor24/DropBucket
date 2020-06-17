cff216da428a3323b49b87aa04e0357b90a9ef30
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-17 21:24:12 +0100

    Updated Auth0 context

--- client/src/contexts/auth0-context.js
+++ client/src/contexts/auth0-context.js
@@ -1,8 +1,9 @@
-import React, { Component, createContext } from 'react';
+import React, { Component, createContext, useContext } from 'react';
 import createAuth0Client from '@auth0/auth0-spa-js';
 
 // create the context
 export const Auth0Context = createContext();
+export const useAuth0 = () => useContext(Auth0Context);
 
 // create a provider
 export class Auth0Provider extends Component {
@@ -25,26 +26,42 @@ export class Auth0Provider extends Component {
   // initialize the auth0 library
   initializeAuth0 = async () => {
     const auth0Client = await createAuth0Client(this.config);
+    this.setState({ auth0Client });
+
+    // check to see if they have been redirected after login
+    if (window.location.search.includes('code=')) {
+      return this.handleRedirectCallback();
+    }
+
     const isAuthenticated = await auth0Client.isAuthenticated();
     const user = isAuthenticated ? await auth0Client.getUser() : null;
+    this.setState({ isLoading: false, isAuthenticated, user });
+  };
 
-    this.setState({ auth0Client, isLoading: false, isAuthenticated, user });
+  handleRedirectCallback = async () => {
+    this.setState({ isLoading: true });
+
+    await this.state.auth0Client.handleRedirectCallback();
+    const user = await this.state.auth0Client.getUser();
+
+    this.setState({ user, isAuthenticated: true, isLoading: false });
+    window.history.replaceState({}, document.title, window.location.pathname);
   };
 
   render() {
-    const { isLoading, isAuthenticated, user } = this.state;
+    const { auth0Client, isLoading, isAuthenticated, user } = this.state;
     const { children } = this.props;
 
     const configObject = {
       isLoading,
       isAuthenticated,
-      user
+      user,
+      loginWithRedirect: (...p) => auth0Client.loginWithRedirect(...p),
+      getTokenSilently: (...p) => auth0Client.getTokenSilently(...p),
+      getIdTokenClaims: (...p) => auth0Client.getIdTokenClaims(...p),
+      logout: (...p) => auth0Client.logout(...p)
     };
 
-    return (
-      <Auth0Context.Provider value={configObject}>
-        {children}
-      </Auth0Context.Provider>
-    );
+    return <Auth0Context.Provider value={configObject}>{children}</Auth0Context.Provider>;
   }
 }
