0fce2882c34876b1e7c2a17f4271d3238fdbce14
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-18 20:54:36 +0100

    Corrected API endpoint and added Auth0 audience

--- client/src/contexts/auth0-context.js
+++ client/src/contexts/auth0-context.js
@@ -16,6 +16,7 @@ export class Auth0Provider extends Component {
   config = {
     domain: process.env.REACT_APP_AUTH0_DOMAIN,
     client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
+    audience: process.env.REACT_APP_AUDIENCE,
     redirect_uri: window.location.origin
   };
 
