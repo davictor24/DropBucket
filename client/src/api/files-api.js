aee4df5148b71579fe0490c144af473f7e5a2ee8
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-18 04:16:47 +0100

    Stubbed files-api.js

---
+++ client/src/api/files-api.js
@@ -0,0 +1,48 @@
+import { apiEndpoint } from '../config'
+import Axios from 'axios'
+
+export async function getFiles(idToken) {
+  console.log('Fetching files')
+
+  const response = await Axios.get(`${apiEndpoint}/files`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${idToken}`
+    },
+  })
+  console.log('Files:', response.data)
+  return response.data.items
+}
+
+export async function uploadFile(idToken,newFile) {
+  const response = await Axios.post(`${apiEndpoint}/files`,  JSON.stringify(newFile), {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${idToken}`
+    }
+  })
+  return response.data.item
+}
+
+export async function deleteFile(idToken, fileId) {
+  await Axios.delete(`${apiEndpoint}/files/${fileId}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${idToken}`
+    }
+  })
+}
+
+export async function getUploadUrl(idToken, todoId) {
+  const response = await Axios.post(`${apiEndpoint}/todos/${todoId}/attachment`, '', {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${idToken}`
+    }
+  })
+  return response.data.uploadUrl
+}
+
+export async function uploadFile_(uploadUrl, file) {
+  await Axios.put(uploadUrl, file)
+}
--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
 const apiId = '...'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+export const domain = 'davictor.auth0.com'
